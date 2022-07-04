91c955f3a0cd2370e99d68e49e12365b5ae5b8a6
princeraghul <raghulraghu2002@gmail.com>
2022-07-04 06:20:37 +0530

    final commit

--- src/components/home.js
+++ src/components/home.js
@@ -13,7 +13,7 @@ function Home({myStorage}) {
         }
     }
   return (
-      <div className='imp'>
+        <div className='imp'>
     <img className='first' src='https://www.brettonwoodsproject.org/wp-content/uploads/2022/02/books.jpeg'/>
     <h3 className='display-4 text-white bg-dark second'>Welcome To Library App</h3>
     <button className='btn btn-info seconds' onClick={handleStart}>Get Started</button>
