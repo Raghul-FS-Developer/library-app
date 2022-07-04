78bf89df051222a6115756147134aa9f93a69a0e
princeraghul <raghulraghu2002@gmail.com>
2022-07-04 06:20:49 +0530

    final commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -80,7 +80,7 @@ const submit =async(e)=>{
     }
   }
   return (
-    <header style={{position:"sticky",top:0}}>
+    <header >
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <a className="navbar-brand text-white ss" to="/">
           <BiLibrary /> Library
